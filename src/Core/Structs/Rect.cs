343332a746ece8f4067c67d75d1cbd8e1279c823
Jakob Popple <jpopple@pditechnologies.com>
2025-11-03 07:50:56 -0600

    add whitespace to rect struct

--- src/Core/Structs/Rect.cs
+++ src/Core/Structs/Rect.cs
@@ -1,9 +1,9 @@
 namespace Weave.Structs;
 
-internal readonly record struct Rect(int X,int Y,int W,int H)
+internal readonly record struct Rect(int x, int y, int w, int h)
 {
-    public int Right => X+W-1;
-    public int Bottom => Y+H-1;
-    public Rect Deflate(int l,int t,int r,int b)
-        => new(X+l, Y+t, Math.Max(0, W-l-r), Math.Max(0, H-t-b));
+    public int Right => x + w - 1;
+    public int Bottom => y + h - 1;
+    public Rect Deflate(int l, int t, int r, int b)
+        => new(x + l, y + t, Math.Max(0, w - l - r), Math.Max(0, h - t - b));
 }
